8566bcdd6c3cb69e54a99284ff3c18b8c1a33f1e
ilham adimuslim <ilhamadim90@gmail.com>
2026-01-23 14:09:12 +0700

    Fix MySQL Railway env & remove localhost DB

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,22 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import fs from "fs";
-import multer from "multer";
 import mysql from "mysql2";
 
 import adminProductRoutes from "./src/routes/adminProductRoutes.js";
 import adminStatsRoutes from "./src/routes/adminStatsRoutes.js";
-import authRoutes from "./src/routes/authRoutes.js";
 import analyzeRoute from "./src/routes/analyzeRoute.js";
+import authRoutes from "./src/routes/authRoutes.js";
 
 /* ================= INIT ================= */
-dotenv.config();
+dotenv.config({ path: "./.env" });
 const app = express();
 const PORT = 5000;
 
 /* ================= MIDDLEWARE ================= */
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
-
+require('dotenv').config({ path: './.env' });
 app.use((req, res, next) => {
   console.log(`📡 [${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
   next();
@@ -29,20 +28,20 @@ if (!fs.existsSync("uploads")) fs.mkdirSync("uploads");
 app.use("/uploads", express.static("uploads"));
 
 /* ================= DATABASE ================= */
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "skinalyze",
-});
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "skinalyze",
+// });
 
-db.connect((err) => {
-  if (err) {
-    console.error("❌ DB Error:", err.message);
-  } else {
-    console.log("✅ MySQL Connected");
-  }
-});
+// db.connect((err) => {
+//   if (err) {
+//     console.error("❌ DB Error:", err.message);
+//   } else {
+//     console.log("✅ MySQL Connected");
+//   }
+// });
 
 /* ================= ROUTES ================= */
 app.use("/api/auth", authRoutes);
